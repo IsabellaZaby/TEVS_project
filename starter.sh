2e418f9c6b1dfc5319d9df7ca2fb7a2b746c6c0b
StefanGass <2010859005@fh-burgenland.at>
2022-04-24 11:20:09 +0200

    Add compile, frontend starter

--- starter.sh
+++ starter.sh
@@ -13,41 +13,57 @@ echo "JDK 11.x (MUST be set as default)"
 echo "Maven"
 echo "RabbitMQ (service MUST run or being started with script)"
 echo "Screen"
-echo
-echo "ATTENTION: Make sure to terminate all running screen sessions before restarting them with this script!"
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+  echo
+  echo "ATTENTION: Make sure to terminate all running screen sessions before restarting them with this script!"
+fi
 echo
 echo "Do you want to continue? (y/n)"
-read continue
+read -r continue
 if [[ $continue != y* ]]; then
-    echo "Bye!"
-    exit 0
+  echo "Bye!"
+  exit 0
 fi
 
 # Works only on Linux...
-#echo
-#echo "Do you want to start to quit all running screen sessions? (y/n"
-#read continueMQ
-#if [[ $continueMQ == y* ]]; then
-#    screen -X -S rabbitMQ kill &> /dev/null
-#    screen -X -S discoveryServer kill &> /dev/null
-#    screen -X -S gateway kill &> /dev/null
-#    screen -X -S serviceOne kill &> /dev/null
-#    screen -X -S serviceTwo kill &> /dev/null
-#    screen -X -S serviceThree kill &> /dev/null
-#fi
-
-echo
-echo "Do you want to start the RabbitMQ server? (y/n"
-read continueMQ
-if [[ $continueMQ == y* ]]; then
-    screen -AmdS rabbitMQ rabbitmq-server
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    screen -X -S rabbitMQ kill &>/dev/null
+    screen -X -S discoveryServer kill &>/dev/null
+    screen -X -S gateway kill &>/dev/null
+    screen -X -S serviceOne kill &>/dev/null
+    screen -X -S serviceTwo kill &>/dev/null
+    screen -X -S serviceThree kill &>/dev/null
+    echo
+    echo "Shut down running screen session."
 fi
 
-cd $path/discovery-server
+echo
+echo "Do you want to start the RabbitMQ server? (y/n)"
+read -r continueMQ
+if [[ $continueMQ == y* ]]; then
+  screen -AmdS rabbitMQ rabbitmq-server
+fi
+
+echo
+echo "Do you want to recompile every service before starting it? (y/n)"
+echo "ATTENTION: This may take a while, so make sure to wait until the script is finished!"
+read -r continueRC
+if [[ $continueRC == y* ]]; then
+  cd "$path" || exit 1
+  mvn clean install
+  cd ./discovery-server || exit 1
+  mvn clean install
+  cd ../gateway || exit 1
+  mvn clean install
+  cd ../service-instance || exit 1
+  mvn clean install
+fi
+
+cd "$path"/discovery-server || exit 1
 screen -AmdS discoveryServer mvn spring-boot:run
-cd ../gateway
+cd ../gateway || exit 1
 screen -AmdS gateway mvn spring-boot:run
-cd ../service-instance
+cd ../service-instance || exit 1
 screen -AmdS serviceOne mvn spring-boot:run -Dspring-boot.run.arguments="--listening.queue=microservice.one --sending.queue=microservice.two --sending.two.queue=microservice.three"
 screen -AmdS serviceTwo mvn spring-boot:run -Dspring-boot.run.jvmArguments=-Dserver.port=9002 -Dspring-boot.run.arguments="--listening.queue=microservice.two --sending.queue=microservice.one --sending.two.queue=microservice.three"
 screen -AmdS serviceThree mvn spring-boot:run -Dspring-boot.run.jvmArguments=-Dserver.port=9003 -Dspring-boot.run.arguments="--listening.queue=microservice.three --sending.queue=microservice.one --sending.two.queue=microservice.two"
@@ -57,6 +73,17 @@ screen -ls
 echo
 echo "To access a running screen session enter \"screen -r session-name\""
 echo "To leave a session without terminating the running service, enter CTRL+a+d inside."
+
+echo
+echo "Do you want to start the client application? (y/n)"
+read -r continueCl
+if [[ $continueCl == y* ]]; then
+  open ../index.html
+  echo
+  echo "Client started in browser."
+  echo "ATTENTION: You have to wait a few seconds, until all the services were started."
+fi
+
 echo
 echo "Bye!"
 
