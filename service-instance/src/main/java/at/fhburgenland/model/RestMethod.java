c186b939c0cb413fbc88e170f9c0e7a0d31f802f
StefanGass <2010859005@fh-burgenland.at>
2022-04-24 11:19:47 +0200

    Add enums

---
+++ service-instance/src/main/java/at/fhburgenland/model/RestMethod.java
@@ -0,0 +1,17 @@
+package at.fhburgenland.model;
+
+/**
+ * @author Stefan Gass, Isabella Zaby
+ * @version 1.0
+ * <p>
+ * Contains Rest HTTP methods (GET, POST, PUT, UPDATE).
+ * @since April 2022
+ */
+public enum RestMethod {
+
+    GET,
+    POST,
+    PUT,
+    DELETE
+
+}
